77bac15093eb983b44bc97048e88707d78266022
Igor Gomes <igorgmsf@gmail.com>
2021-03-24 17:05:56 -0300

    declaração de variaveis

--- js/data.js
+++ js/data.js
@@ -1,3 +1,73 @@
+// Declaração de variáveis
+let curso = [],
+  periodo = [],
+  ra = [],
+  estado = [],
+  cidade = [],
+  genero = [],
+  data_nasc = [],
+  estado_civil = [],
+  port_necessidade = [],
+  conv_deficiente = [],
+  qtd_filhos = [],
+  mora_atualmente = [],
+  qtd_moradores = [],
+  sit_domicilio = [],
+  tempo_moradia = [],
+  faixa_renda = [],
+  qtd_geladeira = [],
+  qtd_celular = [],
+  qtd_microondas = [],
+  qtd_notebooks = [],
+  qtd_lavadoura = [],
+  qtd_moto = [],
+  qtd_automoveis = [],
+  qtd_dvd = [],
+  qtd_televisor = [],
+  qtd_smartphone = [],
+  trabalha = [],
+  vinc_emprego = [],
+  area_emprego = [],
+  horario_trabalho = [],
+  possui_plano = [],
+  escolaridade_pai = [],
+  escolaridade_mae = [],
+  tipo_escola = [],
+  freq_comp = [],
+  util_comp = [],
+  finalidade_comp = [],
+  conhecimento_info = [],
+  conhecimento_windows = [],
+  conhecimento_linux = [],
+  conhecimento_editores = [],
+  conhecimento_planilhas = [],
+  conhecimento_apresentadores = [],
+  conhecimento_erp = [],
+  conhecimento_ingles = [],
+  conhecimento_espanhol = [],
+  conhecimento_outroIdioma = [],
+  info_tv = [],
+  info_internet = [],
+  info_revistas = [],
+  info_jornais = [],
+  info_radio = [],
+  info_redesSociais = [],
+  info_conversas = [],
+  freq_leituraJornal = [],
+  assunto_jornal = [],
+  media_livrosPorAno = [],
+  genero_livro = [],
+  ativ_voluntaria = [],
+  religiao = [],
+  entretenimento_cultural = [],
+  conhecimento_fatec = [],
+  motivo_escolhaCurso = [],
+  expectativa_curso = [],
+  expectativa_posFormatura = [],
+  estudouNaEscolaAntes = [],
+  fez_tecnico = [],
+  meio_transporte = []
+
 var ExcelToJSON = function () {
   this.parseExcel = function (file) {
     var reader = new FileReader();
@@ -13,12 +83,13 @@ var ExcelToJSON = function () {
         var XL_row_object = XLSX.utils.sheet_to_row_object_array(
           workbook.Sheets[sheetName]
         );
-        console.log('antes de virar JSON', XL_row_object)
-        var json_object = JSON.stringify(XL_row_object);
-        console.log(json_object);
-        console.log(typeof(json_object))
-        let teste = JSON.parse(json_object)
-        console.log(teste[0]["1. Qual o seu curso?"])
+        let data = XL_row_object;
+        console.log(data);
+        for(let dados of data) {
+          console.log(dados['6. Qual o seu gênero?'])
+          genero.push(dados['6. Qual o seu gênero?']);
+        }
+        console.log(genero)
       });
     };
 
@@ -27,7 +98,7 @@ var ExcelToJSON = function () {
     };
 
     reader.readAsBinaryString(file);
-    console.log(file)
+    console.log(file);
   };
 };
 
@@ -35,7 +106,9 @@ function handleFileSelect(evt) {
   var files = evt.target.files; // FileList object
   var xl2json = new ExcelToJSON();
   xl2json.parseExcel(files[0]);
-  console.log("evento", files[0])
+  console.log("evento", files[0]);
 }
 
-document.getElementById("inputFile").addEventListener("change", handleFileSelect, false);
+document
+  .getElementById("inputFile")
+  .addEventListener("change", handleFileSelect);
